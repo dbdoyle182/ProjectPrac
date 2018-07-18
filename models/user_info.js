183d970d8ac7ddc5d17e9425b32e1da6a108b9ee
Jongomez <jongomez@comcast.net>
2018-07-18 13:21:47 -0400

    first push

---
+++ models/user_info.js
@@ -0,0 +1,10 @@
+module.exports = function(sequelize, DataTypes) {
+  var User_info = sequelize.define("User_info", {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    phoneNumber: DataTypes.INETEGER(10),
+    token: DataTypes.STRING
+  });
+  return User_info;
+};
